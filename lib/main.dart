492b53eeba4e2377ae3dcf8d572fd31b83fd951f
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2024-08-13 12:44:12 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:expense_tracker/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: MainScreen(),
+      ),
+    ),
+  );
+}
